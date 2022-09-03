83aa9e85e8744974d596677c88d2d842a92203ce
up1 <somkiat.p@gmail.com>
2022-09-03 17:37:17 +0700

    Add new handler

--- workshop/demo-api/cmd/main.go
+++ workshop/demo-api/cmd/main.go
@@ -9,5 +9,6 @@ import (
 func main() {
 	e := echo.New()
 	e.GET("/", hello.HelloHandler)
+	e.POST("/hello", hello.HelloPostHandler)
 	e.Logger.Fatal(e.Start(":1323"))
 }
--- workshop/demo-api/hello/handlers.go
+++ workshop/demo-api/hello/handlers.go
@@ -6,6 +6,28 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type HelloRequest struct {
+	Message string `json:"message"`
+}
+
+type HelloResponse struct {
+	Message string `json:"message"`
+	Code    int64  `json:"code"`
+}
+
+func HelloPostHandler(c echo.Context) error {
+	h := new(HelloRequest)
+	if err := c.Bind(h); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	// Validate input
+	// Call service
+
+	// Create and send response to requester
+	res := HelloResponse{Message: h.Message, Code: 200}
+	return c.JSON(http.StatusOK, res)
+}
+
 func HelloHandler(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+	return c.JSON(http.StatusOK, HelloResponse{})
 }
