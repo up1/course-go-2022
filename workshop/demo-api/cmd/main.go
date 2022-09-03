c3003960596bd75673c93f0e98a320a86251c2ff
up1 <somkiat.p@gmail.com>
2022-09-03 17:52:13 +0700

    Add prometheus middleware

--- workshop/demo-api/cmd/main.go
+++ workshop/demo-api/cmd/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"api/hello"
 
+	"github.com/labstack/echo-contrib/prometheus"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
+	p := prometheus.NewPrometheus("echo", nil)
+    p.Use(e)
 	e.GET("/", hello.HelloHandler)
 	e.POST("/hello", hello.HelloPostHandler)
 	e.Logger.Fatal(e.Start(":1323"))
